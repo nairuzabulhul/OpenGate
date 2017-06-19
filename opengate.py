6a66b3c92f7d97085d2965d5e05983f73c203293
nairuz.ab <nairuz.abulhul@gmail.com>
2017-06-19 10:32:43 -0700

    Added user input to select the vpn by ip address

--- opengate.py
+++ opengate.py
@@ -2,6 +2,29 @@ import csv
 import urllib2, requests
 import   tempfile, subprocess, base64, time
 
+print """
+
+   _,gggggg,_                                           ,gg,                                   
+ ,d8P""d8P"Y8b,                                        i8""8i                    I8            
+,d8'   Y8   "8b,dP                                     `8,,8'                    I8            
+d8'    `Ybaaad88P'                                      `Y88aaad8             88888888         
+8P       `""""Y8                                         d8""""Y8,               I8            
+8b            d8  gg,gggg,     ,ggg,    ,ggg,,ggg,      ,8P     8b   ,gggg,gg    I8     ,ggg,  
+Y8,          ,8P  I8P"  "Yb   i8" "8i  ,8" "8P" "8,     dP      Y8  dP"  "Y8I    I8    i8" "8i 
+`Y8,        ,8P'  I8'    ,8i  I8, ,8I  I8   8I   8I _ ,dP'      I8 i8'    ,8I   ,I8,   I8, ,8I 
+ `Y8b,,__,,d8P'  ,I8 _  ,d8'  `YbadP' ,dP   8I   Yb,"888,,_____,dP,d8,   ,d8b, ,d88b,  `YbadP' 
+   `"Y8888P"'    PI8 YY88888P888P"Y8888P'   8I   `Y8a8P"Y888888P" P"Y8888P"`Y888P""Y88888P"Y888
+                  I8                                                                           
+                  I8                                                                           
+                  I8                                                                           
+                  I8                                                                           
+                  I8                                                                           
+                  I8                                                                           
+
+
+"""
+
+
 url = 'http://www.vpngate.net/api/iphone/'
 
 res = urllib2.urlopen(url)
@@ -9,8 +32,11 @@ res = urllib2.urlopen(url)
 cr = csv.reader(res)
 
 
+country = raw_input("Enter a country : \n")
 
-i = []
+# List of the servers
+servers_list = []
+
 for row in cr:
     #print(row)
 
@@ -18,12 +44,12 @@ for row in cr:
     server = []
     for an in row :
         
-        if 'Japan' in an :
+        if  country in an :
               
               server.append(row[0])
               server.append(row[1])
               server.append(row[5])
-              server.append(row[14])
+              #server.append(row[14])
 ##            print "DDNS Hostname :" ,row[0]
 ##            print "IP Address : " ,row[1]
 ##            print "Score :" ,row[2]
@@ -40,20 +66,38 @@ for row in cr:
 ##            print "\n\n"
               #print server
     if server != []:
-        i.append(server)
+        servers_list.append(server)
+
+
+# List all of the servers and let the user chooses the server
+#user = int(raw_input(">>>: "))
+for server in servers_list :
+
+   # List all servers for the user
+   print server
+
+
+# Ask the user to select a serevr
+user_input = raw_input("Enter VPN IP address: \n")
+for ip in servers_list :
+    
+    if user_input in ip :
+        
+        print 
+        print "[+] VPN SELECTED ", ip
+    else:
+        pass
 
 
 
-#print i[1] 
-
-winner = i[1]
-#print winner
-
-_, path = tempfile.mkstemp()
-
-print path
-
-f = open(path, 'w')
-f.write(base64.b64decode(winner[-1]))
-#f.write('\nscript-security 2\nup /etc/openvpn/update-resolv-conf\ndown /etc/openvpn/update-resolv-conf')
-f.close()
+##winner = i[1]
+###print winner
+##
+##_, path = tempfile.mkstemp()
+##
+##print path
+##
+##f = open(path, 'w')
+##f.write(base64.b64decode(winner[-1]))
+###f.write('\nscript-security 2\nup /etc/openvpn/update-resolv-conf\ndown /etc/openvpn/update-resolv-conf')
+##f.close()
